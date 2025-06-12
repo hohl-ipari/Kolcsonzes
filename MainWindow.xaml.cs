a91462b2f5bac7a88af8d750133104484d0442a4
hohl-ipari <hohl.timea@ipari.vein.hu>
2025-06-12 17:53:00 +0200

    List of laptops and labes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,53 @@ namespace Kolcsonzes
         public MainWindow()
         {
             InitializeComponent();
+            List<Laptop> laptoplista = LoadLaptopList();
+            var letariSzamok = laptoplista.Select(l => l.LeltariSzam)
+                .Distinct()
+                .OrderBy(x => x);
+            foreach (var laptopleltarszam in letariSzamok)
+            {
+                laptopok.Items.Add(laptopleltarszam);
+            }
+        }
+        public List<Laptop> LoadLaptopList()
+        {
+            List<Laptop> lista = new List<Laptop>();
+            StreamReader beolvas = new StreamReader("../../../laptoprentals2022.csv");
+            beolvas.ReadLine();
+            while(!beolvas.EndOfStream)
+            { 
+                string line = beolvas.ReadLine();
+                string[] adatok = line.Split(";");
+                Laptop newLine = new Laptop(adatok[6],
+                                     adatok[7],
+                                     Convert.ToInt32(adatok[9]),
+                                     adatok[10],
+                                     Convert.ToInt32(adatok[11]),
+                                     Convert.ToInt32(adatok[12])
+                                     );
+                lista.Add(newLine);
+            }
+            return lista;
+        }
+        public class Laptop
+        {
+            public string LeltariSzam;
+            public string Model;
+            public int Ram;
+            public string Color;
+            public int Dayfee;
+            public int Deposit;
+
+            public Laptop(string leltariSzam, string model, int ram, string color, int dayfee, int deposit)
+            {
+                LeltariSzam = leltariSzam;
+                Model = model;
+                Ram = ram;
+                Color = color;
+                Dayfee = dayfee;
+                Deposit = deposit;
+            }
         }
     }
 }
